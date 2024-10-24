08a57a7b9520e70f6d59a1d4689adefbf7a1a8c2
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2024-10-24 10:27:00 +0000

    dev: Restore static content loading
    todo:  Integrate mongodb with docker compose

--- app/utils/fake_database.py
+++ app/utils/fake_database.py
@@ -1,10 +1,32 @@
 from json import load
 from app import CategoryExample, Category
-from mongodb_connection import MongoDBConnection
+# from mongodb_connection import MongoDBConnection
 
+
+
+# TODO: Use the MongoDBConnection class instead of reading from JSON
+# You can modify URI or database/collection names if needed
+# mongo_conn = MongoDBConnection()
+# return mongo_conn.load_data()
 
 def load_knowledge_data():
-    # Use the MongoDBConnection class instead of reading from JSON
-    # You can modify URI or database/collection names if needed
-    mongo_conn = MongoDBConnection()
-    return mongo_conn.load_data()
+    with open("./static/reference/knowledge_base_data.json") as f:
+        dataset = load(f)
+        
+    dataset_pyd = {}
+    
+    for category, values in dataset.items():
+        dataset_pyd[category] = []
+        
+        for value in values:
+            examples = value.pop("examples")
+            examples_pyd = [CategoryExample(**item) for item in examples]
+            
+            entry = Category(
+                **value, examples=examples_pyd
+            )
+            dataset_pyd[category].append(entry)
+            
+    return dataset_pyd
+
+
