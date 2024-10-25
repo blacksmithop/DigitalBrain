08bda86c6a01af206fd8847e02bb11a5949465a9
Abhinav KM <angstycoder@gmail.com>
2024-10-25 04:45:07 +0000

    feat: Change import order to prevent cyclic import

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
+from app.utils.models import CategoryExample, Category, DeleteCategory
 from app.utils.fake_database import load_knowledge_data
-from app.utils.models import Category, CategoryExample, DeleteCategory
