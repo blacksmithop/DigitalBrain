1da60d956a649fafb1bf365275584733eacf1952
Abhinav KM <abhinav.m@zerone-consulting.com>
2025-01-21 10:12:10 +0530

    feat: Add content field to BaseModel

--- brain/models/medium.py
+++ brain/models/medium.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
+
 
 class MediumArticle(BaseModel):
     title: str
     publication_date: str
     perma_link: str
+    content: Optional[str] = None
