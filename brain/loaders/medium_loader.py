49c8c2b39e42c5614c318e2fb7b9d8e38f5ce343
Abhinav KM <abhinav.m@zerone-consulting.com>
2025-01-21 09:55:26 +0530

    feat: Add medium loader

--- brain/loaders/medium_loader.py
+++ brain/loaders/medium_loader.py
@@ -1,49 +1,53 @@
 import requests
 import xml.etree.ElementTree as ET
+from brain import MediumArticle
+
 
 # URL of the RSS feed
 RSS_FEED_URL = "https://medium.com/feed"
 
-# Fetch the RSS feed
-def fetch_rss_feed(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.content
-    else:
-        raise Exception(f"Failed to fetch RSS feed. Status code: {response.status_code}")
+class MediumLoader:
+    def __init__(self):
+        pass
 
-# Parse the RSS feed and extract data
-def parse_rss_feed(feed_content):
-    root = ET.fromstring(feed_content)
+    # Fetch the RSS feed
+    def fetch_rss_feed(self, url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.content
+        else:
+            raise Exception(f"Failed to fetch RSS feed. Status code: {response.status_code}")
 
-    items = []
-    for item in root.findall(".//item"):
-        title = item.find("title").text.strip()
-        pub_date = item.find("pubDate").text.strip()
-        guid = item.find("guid").text.strip()
-        items.append({
-            "title": title,
-            "published_on": pub_date,
-            "medium_perma_link": guid
-        })
+    # Parse the RSS feed and extract data
+    def get_medium_articles(self, feed_content):
+        root = ET.fromstring(feed_content)
 
-    return items
+        medium_articles = []
+        for item in root.findall(".//item"):
+            title = item.find("title").text.strip()
+            pub_date = item.find("pubDate").text.strip()
+            guid = item.find("guid").text.strip()
 
-def get_medium_publication_list(username: str):
-    # Fetch and parse the feed
-    try:
-        author_feed_url = f"{RSS_FEED_URL}/@{username}"
-        feed_content = fetch_rss_feed(author_feed_url)
-        parsed_items = parse_rss_feed(feed_content)
+            article = MediumArticle(
+                title=title,
+                publication_date=pub_date,
+                perma_link=guid
+            )
+            medium_articles.append(article)
 
-        # Display the extracted items
-        for item in parsed_items:
-            print(f"Title: {item['Title']}")
-            print(f"Publication Date: {item['Publication Date']}")
-            print(f"GUID: {item['GUID']}")
-            print("-" * 50)
-    except Exception as e:
-        print(f"Error: {e}")
+        return medium_articles
+
+    def initalize(self, username: str):
+        # Fetch and parse the feed
+        try:
+            author_feed_url = f"{RSS_FEED_URL}/@{username}"
+            feed_content = self.fetch_rss_feed(author_feed_url)
+            articles = self.get_medium_articles(feed_content)
+
+            return articles
+        except Exception as e:
+            print(f"Error: {e}")
 
 if __name__ == "__main__":
-    get_medium_publication_list(username="angstycoder101")
+    med = MediumLoader()
+    med.get_medium_articles(username="angstycoder101")
