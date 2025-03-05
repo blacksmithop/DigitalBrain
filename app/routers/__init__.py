4a70029d7edb6b123b63e407b1264443031fef32
Abhinav KM <abhinav.m@zerone-consulting.com>
2025-03-05 11:25:58 +0530

    feat: Import notion router

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,4 +1,5 @@
 from .github import github_router
 from .medium import medium_router
+from .notion import notion_router
 
-router_list = [github_router, medium_router]
+router_list = [github_router, medium_router, notion_router]
