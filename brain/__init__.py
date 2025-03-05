8623909340e65e9eefce02ab49852ee5e5ce4797
Abhinav KM <abhinav.m@zerone-consulting.com>
2025-03-05 11:26:13 +0530

    feat: Import notion loader

--- brain/__init__.py
+++ brain/__init__.py
@@ -1,4 +1,4 @@
 from .environment import GITHUB_TOKEN
-from .loaders import GithubLoader, MediumLoader
+from .loaders import GithubLoader, MediumLoader, NotionLoader
 from .utils import *
 from .models import *
--- brain/loaders/__init__.py
+++ brain/loaders/__init__.py
@@ -1,2 +1,3 @@
 from .github_loader import GithubLoader
 from .medium_loader import MediumLoader
+from .notion_loader import NotionLoader
