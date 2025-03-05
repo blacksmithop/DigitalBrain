55a1bb8c4aa3c1e3e25b76f9625ded06310bf202
Abhinav KM <abhinav.m@zerone-consulting.com>
2025-03-05 11:26:30 +0530

    feat: Change params for Notion unzip logic'

--- brain/loaders/notion_loader.py
+++ brain/loaders/notion_loader.py
@@ -1,13 +1,11 @@
-from utils import unzip_file, identify_notion_export_file
-from os import listdir
+from brain.utils import unzip_file
+
+NOTION_EXPORT_PATH = "./hippocampus/notion"
 
 class NotionLoader:
     def __init__(self):
         pass
     
-    def load_notion_export(self, target_directory: str = "./hippocampus/notion"):
-        directory_files = listdir(target_directory)
-        notion_export_files = identify_notion_export_file(files=directory_files)
-        notion_export = notion_export_files[0]
-        unzip_file(directory_path=target_directory, file_name=notion_export)
-    
+    def load_notion_export(self, file_content: bytes):
+        output_file_path = unzip_file(file_name="notion_export", output_directory=NOTION_EXPORT_PATH, zip_file_content=file_content)
+        return output_file_path
