abee998fd6357565cdabf86f5cca099483d997c5
Abhinav KM <abhinav.m@zerone-consulting.com>
2025-03-05 11:30:08 +0530

    feat: Changed unzip logic

--- brain/utils/unzipper.py
+++ brain/utils/unzipper.py
@@ -1,8 +1,12 @@
 import zipfile
 from os import path
+from io import BytesIO
 
 
-def unzip_file(directory_path: str, file_name: str):
-    file_path = path.join(directory_path, file_name)
-    with zipfile.ZipFile(file_path, "r") as zip_ref:
-        zip_ref.extractall(directory_path)
+def unzip_file(zip_file_content: bytes, file_name: str, output_directory: str):
+    file_path = path.join(output_directory, file_name)
+
+    with BytesIO(zip_file_content) as zip_buffer:
+        with zipfile.ZipFile(zip_buffer, "r") as zip_ref:
+            zip_ref.extractall(file_path)
+    return file_path
